2a5e87b912f5952f16d1db15211432f1a4f768d4
agnichndr <55136630+agnichndr@users.noreply.github.com>
2025-06-17 00:23:50 +0530

    Removing Chat Flow

--- next-web-app/src/app/components/chat/flow/index.ts
+++
@@ -1,6 +0,0 @@
-// src/app/components/chat/flow/index.ts
-
-export { default as RoomSelection } from './RoomSelection';
-export { default as SpaceUpload } from './SpaceUpload';
-export { default as InspirationSelection } from './InspirationSelection';
-export { default as BudgetSelection } from './BudgetSelection';
